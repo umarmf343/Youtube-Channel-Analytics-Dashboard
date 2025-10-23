4ed84dcb26a397d382525ebdcb0880d44a2be99f
umarmf343 <124717827+umarmf343@users.noreply.github.com>
2025-10-23 15:26:21 -0700

    Integrate legacy tools into VidIStream

--- vidstream/lib/utils.ts
+++ vidstream/lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+export { formatNumber } from './utils-advanced'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
